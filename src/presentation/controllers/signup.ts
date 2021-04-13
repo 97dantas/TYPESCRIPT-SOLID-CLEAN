64a1a60cd7ef7155456b670fe708b8671f2d1ccc
Israel Dantas <israel.leite@dasa.com.br>
2021-04-13 14:08:48 -0300

    refactor: add customidez error for missing param

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,10 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     return {
       statusCode: 400,
-      body: new Error('Missing param: name')
+      body: new MissingParamError('name')
     }
   }
 }
